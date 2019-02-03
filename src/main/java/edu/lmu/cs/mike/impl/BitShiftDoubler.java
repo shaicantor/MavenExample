b6ffb8ecdc3284cdbd12365386a7ef0156f9e63f
Shai Cantor <shai@sealights.io>
2019-02-03 17:28:23 +0200

    qrs

--- src/main/java/edu/lmu/cs/mike/impl/BitShiftDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/BitShiftDoubler.java
@@ -17,4 +17,122 @@ public class BitShiftDoubler implements IntegerDoubler {
   public long doDouble3(int i) {
     return 0;
   }
+
+  public long doDouble4(int i) {
+    return 0;
+  }
+
+  public long doDouble5(int i) {
+    return 0;
+  }
+
+  public long doDouble6(int i) {
+    return 0;
+  }
+
+  public long doDouble7(int i) {
+    return 0;
+  }
+
+  public long doDouble8(int i) {
+    return 0;
+  }
+
+  public long doDouble9(int i) {
+    return 0;
+  }
+
+  public long doDouble10(int i) {
+    return 0;
+  }
+
+  public long doDouble11(int i) {
+    return 0;
+  }
+
+  public long doDouble12(int i) {
+    return 0;
+  }
+
+  public long doDouble13(int i) {
+    return 0;
+  }
+
+  public long doDouble14(int i) {
+    return 0;
+  }
+
+  public long doDouble15(int i) {
+    return 0;
+  }
+
+  public long doDouble16(int i) {
+    return 0;
+  }
+
+  public long doDouble17(int i) {
+    return 0;
+  }
+
+  public long doDouble18(int i) {
+    return 0;
+  }
+
+  public long doDouble19(int i) {
+    return 0;
+  }
+
+  public long doDouble20(int i) {
+    return 0;
+  }
+
+  public long doDouble21(int i) {
+    return 0;
+  }
+
+  public long doDouble22(int i) {
+    return 0;
+  }
+
+  public long doDouble23(int i) {
+    return 0;
+  }
+
+  public long doDouble24(int i) {
+    return 0;
+  }
+
+  public long doDouble25(int i) {
+    return 0;
+  }
+
+  public long doDouble26(int i) {
+    return 0;
+  }
+
+  public long doDouble27(int i) {
+    return 0;
+  }
+
+  public long doDouble28(int i) {
+    return 0;
+  }
+
+  public long doDouble29(int i) {
+    return 0;
+  }
+
+  public long doDouble30(int i) {
+    return 0;
+  }
+
+  public long doDouble31(int i) {
+    return 0;
+  }
+
+  public long doDouble32(int i) {
+    return 0;
+  }
+
+
 }
